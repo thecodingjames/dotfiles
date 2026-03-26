2e4ce7f81b4acf296892043106c57a984b5d2dcb
James <16975819+thecodingjames@users.noreply.github.com>
2026-03-26 12:48:02 -0400

    hide all areas in gnome panel to fix 1 pixel bleed

--- .dotfiles-modules/gnome/extensions/hide-panel-lite@thecodingjames/extension.js
+++ .dotfiles-modules/gnome/extensions/hide-panel-lite@thecodingjames/extension.js
@@ -15,27 +15,39 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 
 export default class HidePanel extends Extension {
-    _show_panel() {
+    _allAreas() {
+      return Object.keys(Panel.statusArea)
+    }
+
+    _applyToAreas(action) {
+      this._allAreas.forEach((area) => {
+        Panel.statusArea[area][action]();
+      })
+    }
+
+    _show() {
+      this._applyToAreas('show');
       Panel.set_height(this.panel_height);
     }
     
-    _hide_panel() {
+    _hide() {
+      this._applyToAreas('hide');
       Panel.set_height(0);
     }
 
     enable() {
       this.panel_height = Panel.get_height();
 
-      this._hide_panel();
+      this._hide();
 
-      this.showing = Overview.connect('showing', this._show_panel.bind(this));
-      this.hiding = Overview.connect('hiding', this._hide_panel.bind(this));
+      this.showing = Overview.connect('showing', this._show.bind(this));
+      this.hiding = Overview.connect('hiding', this._hide.bind(this));
     }
 
     disable() {
     	Overview.disconnect(this.showing);
     	Overview.disconnect(this.hiding);
 
-      this._show_panel();
+      this._show();
     }
 }
