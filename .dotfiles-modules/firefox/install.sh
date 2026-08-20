5c91bfaa6f8445d41f7c621663fe383206b5c7a3
James Hoffman <james-hoffman@live.ca>
2026-08-20 15:36:54 -0400

    fix firefox policies

--- .dotfiles-modules/firefox/install.sh
+++ .dotfiles-modules/firefox/install.sh
@@ -1,10 +1,10 @@
 # https://firefox-admin-docs.mozilla.org/reference/policies/
 
 here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-policies=$(cat $here/policies.json)
+policies="$(cat $here/policies.json)"
 
 as_root <<_
-mkdir -p /etc/firefox/policies
-
-echo "$policies" >> /etc/firefox/policies/policies.json
+mkdir -p /etc/firefox/policies; \
+\
+echo "$policies"> /etc/firefox/policies/policies.json;
 _
