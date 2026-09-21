beb98266f48891f9dd9d7c7deec0ead562727364
James Hoffman <james-hoffman@live.ca>
2026-09-21 09:45:33 -0400

    add chruby, add custom vimrc for specific use cases: ruby, sql

--- .dotfiles-modules/ruby/install.sh
+++ .dotfiles-modules/ruby/install.sh
@@ -1,4 +1,5 @@
 ruby_install=$(wget -Sq -O- https://github.com/postmodern/ruby-install/releases/latest 2>&1 | grep Location: | awk -F '/v' '{print $NF}')
+chruby=$(wget -Sq -O- https://github.com/postmodern/chruby/releases/latest 2>&1 | grep Location: | awk -F '/v' '{print $NF}')
 
 as_root <<_
   apt-get install make; \
@@ -6,8 +7,12 @@ as_root <<_
   wget -q -O /tmp/ruby-install.tar.gz https://github.com/postmodern/ruby-install/releases/download/v$ruby_install/ruby-install-${ruby_install}.tar.gz ; \
   cd /tmp && tar -xzf ruby-install.tar.gz; \
   cd /tmp/ruby-install-${ruby_install}/ && make install; \
-  rm -rf /tmp/ruby-install-${ruby_install}; \
+  rm -rf /tmp/ruby-install-${ruby_install} /tmp/ruby-install.tar.gz; \
 \
-  ruby-install --system ruby >/dev/null;
+  ruby-install --system ruby >/dev/null; \
+\
+  wget -q -O /tmp/chruby.tar.gz https://github.com/postmodern/chruby/releases/download/v$chruby/chruby-${chruby}.tar.gz ; \
+  cd /tmp && tar -xzf chruby.tar.gz; \
+  cd /tmp/chruby-${chruby}/ && make install; \
+  rm -rf /tmp/chruby-${chruby} /tmp/chruby.tar.gz;
 _
-	
--- .dotfiles-modules/ruby/source.sh
+++ .dotfiles-modules/ruby/source.sh
@@ -1,2 +1,4 @@
 export PATH=$PATH:$HOME/.gem/bin
 export GEM_HOME=$HOME/.gem
+
+source /usr/local/share/chruby/chruby.sh
